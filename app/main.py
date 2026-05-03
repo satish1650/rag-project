a225f6686be667ada38ba32f6b84033da689df2b
Satish Kumar <satishsjce86@gmail.com>
2026-05-03 18:00:54 +0000

    updated the code

--- app/main.py
+++ app/main.py
@@ -4,11 +4,31 @@ import shutil
 from app.rag_pipeline import build_pipeline
 from app.generator import generate_answer
 
-app = FastAPI()
+app = FastAPI(title="Agentic RAG Pipeline", 
+              description="Agentic Rag Pipeline With Fast API (pdf + Images + Re-ranking)",
+              version="1.0.0")
 agent = None
 
+tags_metadata = [
+    {
+        "name": "Home",
+        "description": "Welcome endpoint",
+    },
+    {
+        "name": "Document Upload",
+        "description": "Upload PDF or files for processing",
+    },
+    {
+        "name": "Query Engine",
+        "description": "Ask questions from uploaded documents",
+    },
+]
 
-@app.post("/upload")
+@app.get("/", tags=["Home"])
+def home():
+    return {"message": "Welcome to Agentic RAG Pipeline"}
+
+@app.post("/upload", tags=["Document Upload"])
 async def upload_pdf(file: UploadFile):
     global agent
 
@@ -21,9 +41,11 @@ async def upload_pdf(file: UploadFile):
 
     return {"status": "PDF processed"}
 
-
-@app.get("/query")
+@app.get("/query", tags=["Query Engine"])
 def query(query: str):
+    if agent is None:
+        return {"error": "Please upload a PDF first"}
+    
     docs = agent.run(query)
     answer = generate_answer(query, docs)
     return {"answer": answer}
